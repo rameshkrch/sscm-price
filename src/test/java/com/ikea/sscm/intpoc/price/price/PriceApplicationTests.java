41e3fe026c8cbf2c2e25cdaaf57d0ed480388eeb
Manikala <jyothi-prasanna-kumar.manikala@capgemini.com>
2023-10-20 11:58:30 +0200

    initial price project

---
+++ src/test/java/com/ikea/sscm/intpoc/price/price/PriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikea.sscm.intpoc.price.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
